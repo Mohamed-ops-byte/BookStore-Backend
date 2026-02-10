92f238e50b7820760872b9ac16a4b5fc052490bf
Mohamed Mostafa <futuresmartsystems@gmail.com>
2026-02-10 19:19:02 +0200

    Lock Symfony 7 for PHP 8.2

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
-    return response()->json([
+    return [
         'status' => 'ok',
         'message' => 'BookStore Backend is running'
-    ]);
+    ];
 });
 
